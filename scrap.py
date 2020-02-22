6337639af3a7af9f087ce8ee8decdcdf97b6ae87
daniel <daniel@192.168.1.128>
2020-02-23 00:03:38 +0100

    Se actualiza el scraper en Python

--- scrap.py
+++ scrap.py
@@ -1,16 +1,22 @@
 import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
+from time import sleep
 
 with open('./data/clothes_uris.txt') as c:
     clothes = c.read().splitlines()
 
-json = [] # an empty list for the JSON we extract
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',}
+json = [] # an empty list for the JSONs
 
 for cloth in clothes:
-    r = requests.get(cloth)
-    soup = BeautifulSoup(r.text,'lxml')
-    json.append(soup.findAll(attrs={"type" : "application/ld+json"})[0].string)
+    r = requests.get(cloth, headers=headers, timeout=10)
+    sleep(1)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text,'lxml')
+        json.append(soup.findAll(attrs={"type" : "application/ld+json"})[0].string)
+    else:
+        json.append("Sorry")
 
 # data analysis
 # df = pd.read_json(json)
